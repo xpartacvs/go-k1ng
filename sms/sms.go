50d4ce179c52f909465d8dc8bb735a5c32d1a8b6
Yudi Primaputra <xpartacvs@gmail.com>
2021-10-25 00:00:58 +0700

    Refactor SMS

--- sms/sms.go
+++ sms/sms.go
@@ -13,9 +13,11 @@ import (
 	"github.com/xpartacvs/go-k1ng/core"
 )
 
+// A type to group several SMS channel
 type Channel string
 
-type sms struct {
+// A main struct of SMS client where you can configure and send SMS.
+type Sms struct {
 	core         core.Core
 	module       core.Module
 	channel      Channel
@@ -25,77 +27,90 @@ type sms struct {
 	template     string
 }
 
-type Sms interface {
-	SetSenderId(senderId string) Sms
-	SetChannel(channelType Channel) Sms
-	SetContent(strContent string) Sms
-	SetTemplate(templateName string) Sms
-	AddDestinations(phoneNumbers ...string) Sms
-	EmptyDestinations() Sms
-	Reset() Sms
-	Send() (*core.Response, error)
-	SendAt(sendTime time.Time) (*core.Response, error)
-}
-
 const (
-	ChannelRegular    Channel = "NON-2FA"
-	ChannelOTP        Channel = "2FA"
-	ChannelDefault    Channel = "DEFAULT"
+	// A dedicated channel for indonesia customer to send non OTP SMS
+	ChannelRegular Channel = "NON-2FA"
+
+	// A dedicated channel for indonesia customer to send OTP SMS
+	ChannelOTP Channel = "2FA"
+
+	// A dedicated channel for international customer to send SMS to indonesian number
+	ChannelDefault Channel = "DEFAULT"
+
+	// A dedicated channel to send SMS using longnumber as it's sender ID
 	ChannelLongNumber Channel = "LONGNUMBER"
 
 	endpointSend string = "api/v1/send"
 )
 
-func create(hosturl, apikey, password string, channelType Channel) (Sms, error) {
+func create(hosturl, apikey, password string, channelType Channel) (*Sms, error) {
 	c, err := core.New(hosturl, apikey, password)
 	if err != nil {
 		return nil, err
 	}
 
-	return &sms{
+	return &Sms{
 		core:    c,
 		channel: channelType,
 		module:  core.ModuleSms,
 	}, nil
 }
 
-func Default(hosturl, apikey, password string) (Sms, error) {
+// Default func is a helper to create a SMS client with `ChannelDefault` as it's preferred channel.
+// Please note that channel is mutable, meaning that you might change it in the future by `SetChannel` method.
+func Default(hosturl, apikey, password string) (*Sms, error) {
 	return create(hosturl, apikey, password, ChannelDefault)
 }
 
-func LongNumber(hosturl, apikey, password string) (Sms, error) {
+// LongNumber func is a helper to create a SMS client with `ChannelLongNumber` as it's preferred channel.
+// Please note that channel is mutable, meaning that you might change it in the future by `SetChannel` method.
+func LongNumber(hosturl, apikey, password string) (*Sms, error) {
 	return create(hosturl, apikey, password, ChannelLongNumber)
 }
 
-func OTP(hosturl, apikey, password string) (Sms, error) {
+// OTP func is s helper to create a SMS client with `ChannelOTP` as it's preferred channel.
+// Please note that channel is mutable, meaning that you might change it in the future by `SetChannel` method.
+func OTP(hosturl, apikey, password string) (*Sms, error) {
 	return create(hosturl, apikey, password, ChannelOTP)
 }
 
-func Regular(hosturl, apikey, password string) (Sms, error) {
+// Regular func is helper to create a SMS client with `ChannelRegular` as it's preferred channel.
+// Please note that channel is mutable, meaning that you might change it in the future by `SetChannel` method.
+func Regular(hosturl, apikey, password string) (*Sms, error) {
 	return create(hosturl, apikey, password, ChannelRegular)
 }
 
-func (s *sms) SetSenderId(senderId string) Sms {
+// SetSenderId assign the given argument as sender ID of the SMS.
+// Please note this method return itself meaning it can be used as chaining-function.
+func (s *Sms) SetSenderId(senderId string) *Sms {
 	s.sid = senderId
 	return s
 }
 
-func (s *sms) SetChannel(channelType Channel) Sms {
+// SetChannel assign the given argument as the main SMS channel.
+// Please note this method return itself meaning it can be used as chaining-function.
+func (s *Sms) SetChannel(channelType Channel) *Sms {
 	s.channel = channelType
 	return s
 }
 
-func (s *sms) SetContent(strContent string) Sms {
+// SetContent assign the given argument as SMS body message.
+// A coma-separated string can be passed as it's argument in order to fill template placeholder
+// Please note this method return itself meaning it can be used as chaining-function.
+func (s *Sms) SetContent(strContent string) *Sms {
 	s.content = strContent
 	return s
 }
 
-func (s *sms) SetTemplate(templateName string) Sms {
+// SetTemplate assign the given argument to be used as the main SMS template.
+// Use `SetContent` method to fill it's placeholder
+// Please note this method return itself meaning it can be used as chaining-function.
+func (s *Sms) SetTemplate(templateName string) *Sms {
 	s.template = templateName
 	return s
 }
 
-func (s *sms) urlValues() (url.Values, error) {
+func (s Sms) urlValues() (url.Values, error) {
 	data := url.Values{}
 	data.Add("module", string(s.module))
 	data.Add("sub_module", string(s.channel))
@@ -122,21 +137,28 @@ func (s *sms) urlValues() (url.Values, error) {
 	return data, nil
 }
 
-func (s *sms) AddDestinations(phoneNumbers ...string) Sms {
+// AddDestination fills the destiantion pool with the given arguments.
+// Please note this method return itself meaning it can be used as chaining-function.
+func (s *Sms) AddDestination(phoneNumbers ...string) *Sms {
 	s.destinations = append(s.destinations, phoneNumbers...)
 	return s
 }
 
-func (s *sms) EmptyDestinations() Sms {
+// EmptyDestination flush the destination pool.
+// Please note this method return itself meaning it can be used as chaining-function.
+func (s *Sms) EmptyDestination() *Sms {
 	s.destinations = nil
 	return s
 }
 
-func (s *sms) Reset() Sms {
-	return s.SetSenderId("").SetContent("").SetTemplate("").EmptyDestinations()
+// Reset will empty the sender id, body message, and destination pool.
+// Please note this method return itself meaning it can be used as chaining-function.
+func (s *Sms) Reset() *Sms {
+	return s.SetSenderId("").SetContent("").SetTemplate("").EmptyDestination()
 }
 
-func (s *sms) Send() (*core.Response, error) {
+// Send the configured SMS to the target in destination pool immediately.
+func (s Sms) Send() (*core.Response, error) {
 	data, err := s.urlValues()
 	if err != nil {
 		return nil, err
@@ -166,7 +188,8 @@ func (s *sms) Send() (*core.Response, error) {
 	return ret, nil
 }
 
-func (s *sms) SendAt(sendTime time.Time) (*core.Response, error) {
+// Send the configured SMS to the target in destination pool at the given argument (time).
+func (s Sms) SendAt(sendTime time.Time) (*core.Response, error) {
 	data, err := s.urlValues()
 	if err != nil {
 		return nil, err
